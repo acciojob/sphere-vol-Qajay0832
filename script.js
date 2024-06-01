2a86bb6aedb76809910e2c3ac3ce33fdca0bd6b8
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-01 12:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function volume_sphere() {
     //Write your code here
-	const radius=document.getElementById("radius");
+	const radiusValue=parseFloat(document.getElementById("radius").value);
 	const volume=document.getElementById("volume");
-	const submit=document.getElementById("submit");
-	submit.addEventListener("click",vol)
-	function vol(){
-		  if (parseInt(radius.value)===NaN || parseInt(radius.value)<0 ){
+		  if (isNaN(radiusValue)|| radiusValue<0 ){
             volume.value=NaN
         }
         else{
-            volume.value=(4/3*Math.PI*parseInt(radius.value)**3).toFixed(4)
+            volume.value=(4/3*Math.PI*radiusValue**3).toFixed(4)
         }
-	}
 	
 	
     
