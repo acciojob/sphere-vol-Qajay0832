aff3720ab8aa0ba29824598a3067fb58fa035cde
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-01 12:38:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radius=document.getElementById("radius");
+	const volume=document.getElementById("volume");
+	const submit=document.getElementById("submit");
+	submit.addEventListener("click",vol)
+	function vol(){
+		volume.value=(4/3*Math.PI*parseInt(radius.value)**3).toFixed(4)
+	}
+	
+	
+    
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
